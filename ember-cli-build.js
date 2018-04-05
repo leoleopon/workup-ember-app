3bc53a531e83b5b29437d31dfdbc56099b174cb4
leoleopon <safari2002@list.ru>
2018-04-06 01:16:53 +0500

    Add some Semantic UI

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -20,5 +20,8 @@ module.exports = function(defaults) {
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
 
+  app.import('bower_components/semantic/dist/semantic.css');
+  app.import('bower_components/semantic/dist/semantic.js');
+
   return app.toTree();
 };
